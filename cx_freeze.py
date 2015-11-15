4799ade7dcf6a546200c1951367d69b124e27d03
Pavel Voropaev <voropaev.p@netcure.com>
2015-11-15 13:58:41 +0400

    Windows Build added

---
+++ cx_freeze.py
@@ -0,0 +1,13 @@
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+include_files=[r"res/"]
+includes=[]
+excludes=[]
+packages=["os", "sys", "re", "jinja2", "argparse"]
+setup(  name = "CAMgen",
+        version = "0.8",
+        description = "Cam file generator",
+        options = {"build_exe": {'excludes':excludes,'packages':packages,'include_files':include_files}},
+        executables = [Executable(script="main.py", base=base)])
